a9d7ced875a863c69ef3d157bc3d2b2dc74efb49
abyanmajid <abyan@abydyl.net>
2024-11-20 15:52:38 +1100

    add: tailwind

---
+++ web/app/(main)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "OpenYan",
+  description: "Free, open-source apps and tooling for students and developers.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`dark ${roboto.className}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
